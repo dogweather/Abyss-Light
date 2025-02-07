73d0b78352392ff7dc3b43c8cd97337ad2953f8d
Robb Shecter <robb@public.law>
2025-02-07 14:11:16 -0700

    tests pass

--- util/test/util_test.py
+++ util/test/util_test.py
@@ -4,7 +4,15 @@ from colorzero.color import Color
 from util import invert_lightness
 
 
-def test_invert_lightness():
-    color = Color(255, 0, 0)
-    inverted_color = invert_lightness(color)
-    assert inverted_color == Color(0, 255, 255)
+def test_make_dark_light():
+    dark_color  = Color.from_hls(h=100, l=0.2, s=0.8)
+    light_color = Color.from_hls(h=100, l=0.8, s=0.8)
+    
+    assert invert_lightness(dark_color).rgb_bytes == light_color.rgb_bytes
+
+
+def test_make_light_dark():
+    light_color = Color.from_hls(h=100, l=0.8, s=0.8)
+    dark_color  = Color.from_hls(h=100, l=0.2, s=0.8)
+    
+    assert invert_lightness(light_color).rgb_bytes == dark_color.rgb_bytes
