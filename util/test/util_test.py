71ba1b2e257b20b0110a64a5657b8d88c8de0f6a
Robb Shecter <robb@public.law>
2025-02-07 14:18:38 -0700

    refactor

--- util/test/util_test.py
+++ util/test/util_test.py
@@ -3,16 +3,13 @@ import pytest
 from colorzero.color import Color
 from util import invert_lightness
 
+DARK  = Color.from_hls(h=100, l=0.2, s=0.8)
+LIGHT = Color.from_hls(h=100, l=0.8, s=0.8)
+    
 
 def test_make_dark_light():
-    dark_color  = Color.from_hls(h=100, l=0.2, s=0.8)
-    light_color = Color.from_hls(h=100, l=0.8, s=0.8)
-    
-    assert invert_lightness(dark_color).html == light_color.html
+    assert invert_lightness(DARK).html == LIGHT.html
 
 
 def test_make_light_dark():
-    light_color = Color.from_hls(h=100, l=0.8, s=0.8)
-    dark_color  = Color.from_hls(h=100, l=0.2, s=0.8)
-    
-    assert invert_lightness(light_color).html == dark_color.html
+    assert invert_lightness(LIGHT).html == DARK.html
