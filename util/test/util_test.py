2dcd33f70d496a2b50d026a4826cd83e83263990
Robb Shecter <robb@public.law>
2025-02-07 16:19:18 -0700

    new passing test

--- util/test/util_test.py
+++ util/test/util_test.py
@@ -3,6 +3,7 @@ from util import invert_lightness
 
 DARK  = Color.from_hls(h=100, l=0.2, s=0.8)
 LIGHT = Color.from_hls(h=100, l=0.8, s=0.8)
+FULL_RED = Color.from_rgb(r=1, g=0, b=0)
     
 
 def test_make_dark_light():
@@ -11,3 +12,7 @@ def test_make_dark_light():
 
 def test_make_light_dark():
     assert invert_lightness(LIGHT).html == DARK.html
+
+
+def test_full_red_does_not_change():
+    assert invert_lightness(FULL_RED).html == FULL_RED.html
