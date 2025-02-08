83fe83b6222bfabd71f92c2303604199f60f9bd6
Robb Shecter <robb@public.law>
2025-02-07 18:20:38 -0700

    refactor

--- util/src/util/__init__.py
+++ util/src/util/__init__.py
@@ -6,13 +6,13 @@ def invert_lightness(color: str) -> str:
     Invert the lightness of a color. Handle both RGB and RGBA colors.
 
     Examples:
-        >>> invert_lightness("#000000")
+        >>> invert_lightness('#000000')
         '#ffffff'
-        >>> invert_lightness("#ffffff")
+        >>> invert_lightness('#ffffff')
         '#000000'
-        >>> invert_lightness("#00000000")
+        >>> invert_lightness('#00000000')
         '#ffffff00'
-        >>> invert_lightness("#ffffff00")
+        >>> invert_lightness('#ffffff00')
         '#00000000'
     """
     rgb, alpha = (color[:-2], color[-2:]) if len(color) == 9 else (color, "")
