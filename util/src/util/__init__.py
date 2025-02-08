fc5a058f097a8d1520826b3ff17503f5090f0f99
Robb Shecter <robb@public.law>
2025-02-07 17:14:44 -0700

    all tests pass

--- util/src/util/__init__.py
+++ util/src/util/__init__.py
@@ -6,16 +6,12 @@ def invert_lightness(color: Color|str) -> Color|str:
         case Color():
             return _invert_lightness(color)
         case str():
-            # Splice off the last two characters (RGBA) if present.
-            if len(color) == 9:
-                rgb   = color[:-2]
-                alpha = color[-2:]
-            else:
-                rgb   = color
-                alpha = ""
+            # Split into RGB and optional alpha components
+            rgb, alpha = (color[:-2], color[-2:]) if len(color) == 9 else (color, "")
             return _invert_lightness(Color.from_string(rgb)).html + alpha
 
 
 def _invert_lightness(color: Color) -> Color:
     orig_hls = color.hls
     return Color.from_hls(h=orig_hls.h, l=1 - orig_hls.l, s=orig_hls.s)
+    
